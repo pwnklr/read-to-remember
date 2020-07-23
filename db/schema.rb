981bbfcdaae66ec643367485e5bbc99377c6ecdc
iva mudronja <iva.pilizota@gmail.com>
2020-07-23 20:07:04 +0200

    all models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_165158) do
+ActiveRecord::Schema.define(version: 2020_07_23_175857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "highlights", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "source_id", null: false
+    t.text "content"
+    t.string "page"
+    t.boolean "favorite", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["source_id"], name: "index_highlights_on_source_id"
+    t.index ["user_id"], name: "index_highlights_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.bigint "highlight_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["highlight_id"], name: "index_notes_on_highlight_id"
+  end
+
+  create_table "sources", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "title"
+    t.string "publishing_year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_sources_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +63,8 @@ ActiveRecord::Schema.define(version: 2020_07_23_165158) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "highlights", "sources"
+  add_foreign_key "highlights", "users"
+  add_foreign_key "notes", "highlights"
+  add_foreign_key "sources", "authors"
 end
