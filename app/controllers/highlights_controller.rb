473df25d6e27816096ef06dd5b70b931a9de8799
iva mudronja <iva.pilizota@gmail.com>
2020-07-25 15:01:32 +0200

    wip

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,4 +1,10 @@
 class HighlightsController < ApplicationController
+
+  def flashcards # check
+    @highlights = Highlight.all
+    @flashcards = Array.new(7, @highlights.sample)
+  end
+
   def highlight_params
     params.require(:highlight).permit(:content, :page, :favorite, :source_id, :user_id, :tag_list)
   end
