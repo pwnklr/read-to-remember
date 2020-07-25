d83af5814f7fab9928aaf162b56455a5aabac804
iva mudronja <iva.pilizota@gmail.com>
2020-07-25 15:57:56 +0200

    wip

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,8 +1,40 @@
 class HighlightsController < ApplicationController
+  before_action :set_highlight, only: [:show, :destroy]
 
-  def flashcards # check
+  def index
+    @highlights = Highlight.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @highlight = Highlight.new
+  end
+
+  def create
+    @highlight = Highlight.new(highlight_params)
+    if @highlight.save
+      redirect_to highlights_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @highlight.destroy
+    redirect_to highlights_path, notice: "Highlight was succsesfully removed!"
+  end
+
+  def flashcards
     @highlights = Highlight.all
     @flashcards = Array.new(7, @highlights.sample)
+  end
+
+  private
+
+  def set_highlight
+    @highlight = Highlight.find(params[:id])
   end
 
   def highlight_params
