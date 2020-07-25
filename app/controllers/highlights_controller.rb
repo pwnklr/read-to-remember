cf281bf8c4612b8dd98c392dbc1d5597d5a8330e
iva mudronja <iva.pilizota@gmail.com>
2020-07-25 17:49:42 +0200

    highlights controller - basic methods + flashcards + favorites

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -1,15 +1,19 @@
 class HighlightsController < ApplicationController
-  before_action :set_highlight, only: [:show, :destroy]
+  before_action :set_highlight, only: [:show, :edit, :update, :destroy]
+  before_action :set_source, only: [:new, :create]
 
   def index
     @highlights = Highlight.all.order(created_at: :desc)
   end
 
+  # i think we don't need this one
   def show
   end
 
   def new
     @highlight = Highlight.new
+    @highlight.user = current_user
+    @highlight.source = @source
   end
 
   def create
@@ -21,6 +25,19 @@ class HighlightsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    # just favorite & tags # wip
+    @highlight.update(fav_tag_param)
+    if @highlight.save
+      redirect_to highlights_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @highlight.destroy
     redirect_to highlights_path, notice: "Highlight was succsesfully removed!"
@@ -28,7 +45,15 @@ class HighlightsController < ApplicationController
 
   def flashcards
     @highlights = Highlight.all
-    @flashcards = Array.new(7, @highlights.sample)
+    @flashcards = []
+    while @flashcards.length < 7 do
+      @flashcards <<  @highlights.sample
+      @flashcards.uniq!
+    end
+  end
+
+  def favorites
+    @highlights = Highlight.where(favorite: true)
   end
 
   private
@@ -37,7 +62,15 @@ class HighlightsController < ApplicationController
     @highlight = Highlight.find(params[:id])
   end
 
+  def set_source
+    @source = Source.find(params[:source_id])
+  end
+
   def highlight_params
     params.require(:highlight).permit(:content, :page, :favorite, :source_id, :user_id, :tag_list)
+  end
+
+  def fav_tag_param
+    params.require(:highlight).permit(:favorite, :tag_list)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :highlights, only: [:index, :show, :delete, :new, :create] do
+  resources :highlights do
     collection do
       get :flashcards
+      get :favorites
     end
   end
   resources :users, only: [:show]
