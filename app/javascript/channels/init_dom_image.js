5c472394ab7a7177a671528173f36d4fb74a118b
iva mudronja <iva.pilizota@gmail.com>
2021-03-17 15:10:16 +0100

    create img from highlight on btn click

--- app/javascript/channels/init_dom_image.js
+++ app/javascript/channels/init_dom_image.js
@@ -1,35 +1,38 @@
-
-
-import domtoimage from 'dom-to-image';
-
-//var domtoimage = require('dom-to-image');
-
 const initDomImage = () => {
 
+  var btns = document.querySelectorAll('.my-btns');
 
+  if(btns) {
 
-var dataUrl = 'https://stackoverflow.com/questions/41303634/failed-to-execute-appendchild-on-node-only-one-element-on-document-allowed'
-var node = document.getElementById('my-node');
-var btn = document.getElementById('my-btn');
+    for(let i = 0; i < btns.length; i++) {
 
-if(btn) {
+      btns[i].addEventListener('click', function() {
+        const id = btns[i].dataset.cardId;
 
-  btn.addEventListener('click', function() {
+        const cardW = document.getElementById(`card-${id}`);
+        const card = document.getElementById(`image-${id}`);
 
+        var node = document.getElementById('my-node');
+        const canvas = document.createElement('canvas');
 
-domtoimage.toPng(node)
-    .then (function (dataUrl) {
-        var img = new Image();
-        img.src = dataUrl;
-        document.body.appendChild(img);
-    })
-    .catch(function (error) {
-        console.error('oops, something went wrong!', error);
-    });
+        const ctx = canvas.getContext('2d');
+        canvas.width = cardW.offsetWidth;
+        canvas.height = cardW.offsetHeight;
 
-  });
+        const tempImg = document.createElement('img');
+        tempImg.addEventListener('load', onTempImageLoad);
 
-}
+        tempImg.src = 'data:image/svg+xml,' + encodeURIComponent(`<svg xmlns="http://www.w3.org/2000/svg"><foreignObject width="100%" height="100%"><div xmlns="http://www.w3.org/1999/xhtml"><style>#d {padding: 12px 24px; background-color: #fff;}</style><div id='d'>${card.innerHTML}</div></div></foreignObject></svg>`)
+        const targetImg = document.createElement('img');
+        node.appendChild(targetImg);
+
+        function onTempImageLoad(e) {
+          ctx.drawImage(e.target, 0, 0)
+          targetImg.src = canvas.toDataURL()
+        }
+      });
+    }
+  }
 }
 
 export { initDomImage };
